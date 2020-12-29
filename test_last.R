8d3032f7b63165ceaa61cffc3a34f9494ff861aa
HiphopZeus <294374790@qq.com>
2020-12-29 23:04:50 +0000

    no

--- test_last.R
+++ test_last.R
@@ -1,1 +1,10 @@
 #test for new
+write_compendium_description(package = "binder_test", 
+                             description = "for the test")
+
+write_dockerfile(maintainer = "mhj") 
+
+generate_badge() 
+
+# copy and paste the code generate_badge produces 
+#into the file (e.g. README.md / a .Rmd) of your choice.
